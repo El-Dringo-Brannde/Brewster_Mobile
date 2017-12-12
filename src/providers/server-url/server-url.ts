27c0bdf4fa7cb7b8268d1ffee4de67475dfd340b
Brandon Lee Dring <Brandon.d.junk@gmail.com>
2017-12-12 00:20:26 -0800

    Added logic to display beer details

---
+++ src/providers/server-url/server-url.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the ServerUrlProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class ServerUrlProvider {
+   public serverURL: string = 'http://localhost:3000';
+   constructor(public http: HttpClient) {
+      console.log('Hello ServerUrlProvider Provider');
+   }
+
+   url() {
+      return this.serverURL;
+   }
+
+}
